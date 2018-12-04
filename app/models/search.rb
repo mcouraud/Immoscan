a2dc109d5498700fe41dd42fe0ccaa25e346d1a0
Mathilde Couraud <mcouraud@outlook.fr>
2018-12-04 15:38:47 +0100

    Models done

---
+++ app/models/search.rb
@@ -0,0 +1,5 @@
+class Search < ApplicationRecord
+  belongs_to :user
+
+  validates :mail_alert, :search_url , presence: true
+end
---
+++ db/migrate/20181204141933_create_flats.rb
@@ -0,0 +1,24 @@
+class CreateFlats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :flats do |t|
+      t.string :address
+      t.string :city
+      t.string :zipcode
+      t.string :full_address
+      t.string :surface_housing
+      t.string :surface_ground
+      t.string :nb_rooms
+      t.string :nb_bedrooms
+      t.string :price
+      t.string :buy_rent
+      t.text Array :photos
+      t.text :description
+      t.string :furnished
+      t.string :type_advert
+      t.string :website_source
+      t.string :ad_url
+
+      t.timestamps
+    end
+  end
+end
