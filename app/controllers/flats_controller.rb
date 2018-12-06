7817225cc59f10427a4e1e01e5d81957b29c11a3
Mathilde Couraud <mcouraud@outlook.fr>
2018-12-06 11:49:43 +0100

    Barre de recherche page d'accueil terminée (back-end)

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,12 +4,15 @@ class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:city_query].present?  && params[:price_query].present? && params[:nb_rooms_query].present?
-      @flats = Flat.where('city ILIKE ?', params[:city_query]).where('price <= ?', params[:price_query].to_i).where('nb_rooms = ?', params[:nb_rooms_query].to_i)
-    elsif params[:city_query].present?  && params[:price_query].present?
-      @flats = Flat.where('city ILIKE ?', params[:city_query]).where('price <= ?', params[:price_query].to_i)
-    elsif params[:city_query].present?
-      @flats = Flat.where('city ILIKE ?', params[:city_query])
+    if params[:city].present?  && params[:price].present? && params[:nb_rooms].present? && params[:rent_or_buy].present?
+      @flats = Flat.where('city ILIKE ?', params[:city]).where('price <= ?', params[:price].to_i).where('nb_rooms = ?', params[:nb_rooms].to_i).where('rent_or_buy ILIKE ?', params[:rent_or_buy])
+    elsif params[:city].present?  && params[:price].present? && params[:rent_or_buy].present?
+      @flats = Flat.where('city ILIKE ?', params[:city]).where('price <= ?', params[:price].to_i).where('rent_or_buy ILIKE ?', params[:rent_or_buy])
+    elsif params[:city].present? && params[:rent_or_buy].present?
+      @flats = Flat.where('city ILIKE ?', params[:city]).where('rent_or_buy ILIKE ?', params[:rent_or_buy])
+    else
+      flash[:alert] = "Veuillez choisir une localisation"
+      redirect_to root_path
     end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Flat.destroy_all
 
-flat_marseille = Flat.create([{city: "Marseille", address: "21 rue berlioz", price: 400, nb_rooms: 3}, {city: "Marseille", address: "137 rue de rome", price: 300, nb_rooms: 2}, {city: "Marseille", address: "7 rue crudère", price: 350, nb_rooms: 2}])
+flat_marseille = Flat.create([{city: "Marseille", address: "21 rue berlioz", price: 400, nb_rooms: 3, rent_or_buy: "louer"}, {city: "Marseille", address: "137 rue de rome", price: 300, nb_rooms: 2, rent_or_buy: "louer"}, {city: "Marseille", address: "7 rue crudère", price: 200000, nb_rooms: 3, rent_or_buy: "acheter"}])
 
 flat_lyon = Flat.create([{city: "Lyon", address: "95 boulevard de la croix rousse"}, {city: "Lyon", address: "252 rue andré philip"}])
