6dbade217524d76f8e80bc5bb25930a9c4b4026f
Mathilde Couraud <mcouraud@outlook.fr>
2018-12-06 18:58:20 +0100

    homepage v2

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,9 @@
 class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-  include ApplicationHelper
+  # include ApplicationHelper
 
   def index
-    ScrapingPapJob.perfom_now
+    # ScrapingPapJob.perfom_now
     if params[:city].present?  && params[:price].present? && params[:nb_rooms].present? && params[:rent_or_buy].present?
       @flats = Flat.where('city ILIKE ?', params[:city]).where('price <= ?', params[:price].to_i).where('nb_rooms = ?', params[:nb_rooms].to_i).where('rent_or_buy ILIKE ?', params[:rent_or_buy])
     elsif params[:city].present?  && params[:price].present? && params[:rent_or_buy].present?
