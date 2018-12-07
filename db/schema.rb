6890d5a639632904065d466d0e1e08e8c37b982c
Thomas Demoncy <demoncy.thomas@gmail.com>
2018-12-07 11:07:01 +0100

    model created et seeds done

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_07_093047) do
+ActiveRecord::Schema.define(version: 2018_12_07_100259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
-    t.string "cp"
     t.string "ci"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
